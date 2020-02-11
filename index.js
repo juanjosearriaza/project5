d23f03548002ac125c1195e3940f91c13bf3af0a
juanjosearriaza <juanjosearriaza@hotmail.com>
2020-02-11 18:08:15 +0000

    add modified files

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const images = document.getElementsByClassName("teddycarousel")
 
 
-const URL = "http://127.0.0.1:5500/index.html" + "?name=Norbert" + "&name=Arnold" + "&name=LennyandCarl" + "&name=Gustav" + "name=Garfunkel"
-
-const a = window.document.createElement("a")
-
-a.href = URL
-let params = new URLSearchParams(a.search)
-console.log(params);
 
 
 
@@ -18,16 +11,17 @@ function getNames() {
   fetch('http://localhost:3000/api/teddies')
 
     .then((res) => res.json())
-    .then((data) => {  
+    .then((data) => {
 
-      
+
       let html = ``;
-      
-      
+
+
       for (i = 0; i < data.length; i++) {
-   
+
         images[i].src = data[i].imageUrl;
 
+        let queryString = '?id=' + data[i]._id;
 
         html += `<div class="col-12 col-lg-4">
   <div class="card mb-4 mb-lg-0">
@@ -35,17 +29,19 @@ function getNames() {
       <div class="card-body text-center">
           <h5 class="card-title teddy">${data[i].name}</h5>
           <h6 class="card-title price">Price: $ ${data[i].price}</h6>
-          <a class="btn btn-primary" href="teddy1.html">See Article</a>
+          <a class="btn btn-primary" href="detail.html${queryString}">See Article</a>
       </div>
   </div>
   </div>`;
-  
-        }
+
+      }
 
       document.querySelector(".createteddys").innerHTML = html;
-      
-      
+
+
     })
 }
 
 getNames();
+
+
--- teddy1.js
+++ teddy1.js
@@ -8,27 +8,36 @@ const colorchocolate = document.getElementsByClassName("btn-chocolate");
 const colorwhite = document.getElementsByClassName("btn-light");
 const colorblack = document.getElementsByClassName("btn-dark");
 
-function getNames () {fetch('http://localhost:3000/api/teddies')
+let id = location.search
+
+id = id.replace("?id=","");
+
+
+let URL = 'http://localhost:3000/api/teddies/'+ id
+
+console.log(URL);
+
+function getNames () {fetch(URL)
 .then((res) => res.json())
 .then((data) => {  
 
     let output = "Price: USD: "
     let output1 = "<strong> Description: </strong>"
 
-    for (i = 0; i < data.length; i++) {
+    
 
-    image[0].src = data[0].imageUrl;
-    name.textContent = data[0].name;
-    code[0].textContent = data[0]._id;
-    price[0].textContent = output + data[0].price;
-    description[0].innerHTML = output1 + data[0].description;
-    colortan[0].textContent = data[0].colors[0];
-    colorchocolate[0].textContent = data[0].colors[1];
-    colorwhite[0].textContent = data[0].colors[2];
-    colorblack[0].textContent = data[0].colors[3];
+    image[0].src = data.imageUrl;
+    name.textContent = data.name;
+    code[0].textContent = data._id;
+    price[0].textContent = output + data.price;
+    description[0].innerHTML = output1 + data.description;
+    colortan[0].textContent = data.colors[0];
+    colorchocolate[0].textContent = data.colors[1];
+    colorwhite[0].textContent = data.colors[2];
+    colorblack[0].textContent = data.colors[3];
 
     
-    }
+    
 })
 
 }
