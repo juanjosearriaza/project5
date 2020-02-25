47e71351eaf34de0f5708f7347fbafb2cd0c6a06
juanjosearriaza <juanjosearriaza@hotmail.com>
2020-02-25 18:05:41 +0000

    add modified files

--- detail.js
+++ detail.js
@@ -10,114 +10,97 @@ const colorblack = document.getElementsByClassName("btn-dark");
 const addToCart = document.querySelector(".cart");
 const input = document.querySelector("input");
 
-
-
-let id = location.search
+let id = location.search;
 
 id = id.replace("?id=", "");
 
-
-let URL = 'http://localhost:3000/api/teddies/' + id
-
-
+let URL = "http://localhost:3000/api/teddies/" + id;
 
 function getNames() {
-    fetch(URL)
-        .then((res) => res.json())
-        .then((data) => {
+  fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+      let output = "Price: USD: ";
+      let output1 = "<strong> Description: </strong>";
 
-            let output = "Price: USD: "
-            let output1 = "<strong> Description: </strong>"
-
-
-
-            image[0].src = data.imageUrl;
-            name.textContent = data.name;
-            code[0].textContent = data._id;
-            price[0].textContent = output + data.price;
-            description[0].innerHTML = output1 + data.description;
-            colortan[0].textContent = data.colors[0];
-            colorchocolate[0].textContent = data.colors[1];
-            colorwhite[0].textContent = data.colors[2];
-            colorblack[0].textContent = data.colors[3];
-
-
-
-        })
-
+      image[0].src = data.imageUrl;
+      name.textContent = data.name;
+      code[0].textContent = data._id;
+      price[0].textContent = output + data.price;
+      description[0].innerHTML = output1 + data.description;
+      colortan[0].textContent = data.colors[0];
+      colorchocolate[0].textContent = data.colors[1];
+      colorwhite[0].textContent = data.colors[2];
+      colorblack[0].textContent = data.colors[3];
+    });
 }
 
 getNames();
 
 addToCart.addEventListener("click", () => {
-    addItems();
-})
-
-
+  addItems();
+});
 
 function onLoadCart() {
-    let productNumbers = localStorage.getItem("productNumbers");
+  let productNumbers = localStorage.getItem("productNumbers");
 
-    if (productNumbers) {
-
-        document.querySelector(".cartspan span").textContent = productNumbers
-    }
+  if (productNumbers) {
+    document.querySelector(".cartspan span").textContent = productNumbers;
+  }
 }
-
 
 function addItems() {
-    fetch(URL)
-        .then((res) => res.json())
-        .then((data) => {
-            let cart = localStorage.getItem("cart") || []
-            let productNumbers = localStorage.getItem("productNumbers")
-            productNumbers = parseInt(productNumbers)
+  fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+      let cart = localStorage.getItem("cart") || [];
 
+      if (typeof cart === "string") {
+        cart = JSON.parse(cart);
+      }
+      let find;
+      for (let i = 0; i < cart.length; i++) {
+          console.log(cart[i].id);
+        if (cart[i].id == id) {
+          find = i;
+          break
+        }
+      }
+console.log(find);
+      //TO DO
+      //Find out if the teddy bear is there and if it´s there increase the quantity
+      document.querySelector(".cartspan span").textContent = parseInt(
+        input.value
+      );
 
-
-            if (typeof cart === "string") {
-                cart = JSON.parse(cart);
-            }
-
-            //TO DO 
-            //Find out if the teddy bear is there and if it´s there increase the quantity
-            document.querySelector(".cartspan span").textContent = parseInt(input.value)
-
-
-
-
-            if (cart.id) {
-
-
-                cart[id].quantity = cart[id].quantity + parseInt(input.value)
-                cart[id].quantity = parseInt(input.value)
-            } else {
-
-
-                cart.push({
-                    quantity: parseInt(input.value),
-                    name: data.name,
-                    price: data.price,
-                    id: id
-
-                })
-                localStorage.setItem("cart", JSON.stringify(cart))
-                
-            }
-            if (productNumbers) {
-                localStorage.setItem("productNumbers", parseInt(input.value) + productNumbers)
-                document.querySelector(".cartspan span").textContent = parseInt(input.value) + productNumbers
-            } else {
-                localStorage.setItem("productNumbers", parseInt(input.value))
-                document.querySelector(".cartspan span").textContent = parseInt(input.value)
-            }
-
-
-
-        })
+      if (find) {
+        cart[find].quantity = parseInt(input.value);
+        
+      } else {
+        cart.push({
+          quantity: parseInt(input.value),
+          name: data.name,
+          price: data.price,
+          id: id
+        });
+        
+      }
+      localStorage.removeItem("cart")
+      localStorage.setItem("cart", JSON.stringify(cart));
+      /*if (productNumbers) {
+        localStorage.setItem(
+          "productNumbers",
+          parseInt(input.value) + productNumbers
+        );
+        document.querySelector(".cartspan span").textContent =
+          parseInt(input.value) + productNumbers;
+      } else {
+        localStorage.setItem("productNumbers", parseInt(input.value));
+        document.querySelector(".cartspan span").textContent = parseInt(
+          input.value
+        );
+      }*/
+    });
 }
 
-
-
 onLoadCart();
-
