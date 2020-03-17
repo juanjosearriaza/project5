eb7011cb637be51d353dde61fa6ae6b8442c852f
juanjosearriaza <juanjosearriaza@hotmail.com>
2020-03-17 18:18:04 +0000

    add modified files

--- cart.js
+++ cart.js
@@ -1,4 +1,4 @@
-let finishshopping = document.getElementById("finish");
+let finishshopping = document.querySelector("#finish");
 let firstName = document.getElementById("firstname");
 let lastName = document.getElementById("lastname");
 let email = document.getElementById("email");
@@ -7,17 +7,26 @@ let postCode = document.getElementById("postcode");
 let city = document.getElementById("city");
 let erase = document.getElementsByClassName("fa-window-close");
 
+console.log(erase);
+
 for (let i = 0; i < erase.length; i++) {
-  erase[i].addEventListener("click", function() {
-    console.log("hola");
-  });
+  erase[i].addEventListener("click", function () {
+    console.log("hello");
+  })
 }
+
+
+
 
 function getTotalCost() {
   let cart = localStorage.getItem("cart") || [];
-  cart = JSON.parse(cart);
+  
+  if (typeof cart === "string") {
+    cart = JSON.parse(cart);
+  }
   let totalCost = 0;
   let html = ``;
+
 
   for (let i = 0; i < cart.length; i++) {
     totalCost += cart[i].quantity * cart[i].price;
@@ -28,17 +37,20 @@ function getTotalCost() {
         cart[i].image
       }" alt="teddy" style="height:70px;width:50px;"></div>
       <div class="col-3 mt-5">${cart[i].name}</div>
-      <div class="col-3 mt-5 text-center"><input type="number" value="${
+      <div class="col-3 mt-5 text-center"><input class="quantity" type="number" value="${
         cart[i].quantity
       }" min="1" style="width:45px"></div>
       <div class="col-3 mt-5 text-center">$${cart[i].price / 100}.00</div>
       `;
+      
   }
   document.querySelector(".createcart").innerHTML = html;
   document.querySelector(".price").textContent =
     "Total to pay  $" + totalCost / 100 + ".00";
 
-  console.log(cart);
+
+
+  console.log(finishshopping);
   console.log(totalCost);
 }
 
@@ -54,8 +66,9 @@ function onLoadCart() {
 
 onLoadCart();
 
-finishshopping.addEventListener("click", function() {
-  let productId = []
+finishshopping.addEventListener("click", function(event) {
+  event.preventDefault();
+  let productId = [];
   let contact = {
     firstName: firstName.value,
     lastName: lastName.value,
@@ -67,21 +80,24 @@ finishshopping.addEventListener("click", function() {
 
   let cart = localStorage.getItem("cart") || [];
   cart = JSON.parse(cart);
-  
-  for (let i = 0; i < cart.length; i++ ) {
-    
-    productId.push(cart[i].id)
-  }
 
+  for (let i = 0; i < cart.length; i++) {
+    productId.push(cart[i].id);
+  }
 
   fetch("http://localhost:3000/api/teddies/order", {
     method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify({contact: contact, products: productId})
+    body: JSON.stringify({ contact: contact, products: productId })
   })
     .then(response => response.json())
-    .then(json => console.log(json));
+    .then(json => {
+      
+      localStorage.setItem("orderId", JSON.stringify(json));
+      location.href = "confirmation.html"
+      localStorage.clear()}
+    );
 });
---
+++ confirmation.js
@@ -0,0 +1,13 @@
+let orderId = localStorage.getItem("orderId")
+orderId = JSON.parse(orderId)
+let totalCost = 0;
+let cart = localStorage.getItem("cart") || [];
+  cart = JSON.parse(cart);
+
+for (let i = 0; i < cart.length; i++) {
+    totalCost += cart[i].quantity * cart[i].price;
+
+    document.querySelector(".col-12").innerHTML = "Thanks for shopping with us, your orderId is " + orderId.orderId + " and the total cost is $" + totalCost/100 +".00."
+  }
+
+
